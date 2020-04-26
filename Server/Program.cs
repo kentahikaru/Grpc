43910fcfb1d1d2d38c297fe6e5e05381f54c801f
Lukáš Šandala <lukas.sandala@ixtent.com>
2020-04-26 21:36:33 +0200

    Initial commit

---
+++ Server/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace Server
+{
+    class Program
+    {
+    const int port = 50051;
+        static void Main(string[] args)
+        {
+            Grpc.Core.Server server = null;
+            try
+            {
+                server = new Grpc.Core.Server()
+                {
+                    Ports = {new ServerPort("localhost", port, ServerCredentials.Insecure) }
+                };
+                server.Start();
+                Console.WriteLine("Server is listening on port: " + port);
+                Console.ReadKey();
+            }
+            catch(IOException ex)
+            {
+                Console.WriteLine("server did not start on port: " + port);
+                throw;
+            }
+            finally
+            {
+                if(server != null)
+                {
+                    server.ShutdownAsync().Wait();
+                }
+            }
+        }
+    }
+}
